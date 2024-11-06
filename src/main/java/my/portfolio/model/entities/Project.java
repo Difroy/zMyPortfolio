e334fb277cce779b207001c3e7865f91535b3f71
Difroy <unifroy@gmail.com>
2024-11-06 23:21:18 +0100

    1

--- src/main/java/my/portfolio/model/entities/Project.java
+++ src/main/java/my/portfolio/model/entities/Project.java
@@ -1,9 +1,21 @@
 package my.portfolio.model.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Project {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
 	
 	
-	
+	String name;
+	String description;
+	String img;
+	Tech tech;
 	
 }
---
+++ src/main/java/my/portfolio/model/entities/Tech.java
@@ -0,0 +1,9 @@
+package my.portfolio.model.entities;
+
+public enum Tech {
+
+	HTML, CSS, JAVASCRIPT, JAVA, MYSQL, ANGULAR, TYPESCRIPT, BOOTSTRAP, SPRING;
+	
+	
+	
+}
